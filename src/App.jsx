dd6f7c7dbcdce8baa389a3e340091d22c5ffcc6f
Umar Khan <khanumar0108@gmail.com>
2024-05-13 15:26:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Play game</h1>
+        <button>Play!</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
